65952c155dd2bb89e24b3a7f840d03badd3192ed
ala-kad <kaddechiala@gmail.com>
2025-07-12 19:18:07 +0100

    feat: imported nav component into app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavComponent
+],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
